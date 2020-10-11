7a20d47c758fa4b567e2dbb909a2f63d92818be4
300069965 <abhishyam.chennayipalem@myntra.com>
2020-10-11 20:20:08 +0530

    Hello world example complete

--- src/main/java/com/abhishyam/learn/controller/HelloWorldController.java
+++ src/main/java/com/abhishyam/learn/controller/HelloWorldController.java
@@ -1,7 +1,19 @@
 package com.abhishyam.learn.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * @author Abhishyam.c on 11/10/20
  */
+
+@Controller
 public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld(Model model){
+        model.addAttribute("message", "Hello World");
+        return "helloworld";
+    }
 }
