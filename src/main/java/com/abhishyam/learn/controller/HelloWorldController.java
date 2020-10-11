9ae02b2d4f6277ae739c32431dc4875fe0bb6caa
300069965 <abhishyam.chennayipalem@myntra.com>
2020-10-11 19:49:10 +0530

    first commit

---
+++ src/main/java/com/abhishyam/learn/controller/HelloWorldController.java
@@ -0,0 +1,7 @@
+package com.abhishyam.learn.controller;
+
+/**
+ * @author Abhishyam.c on 11/10/20
+ */
+public class HelloWorldController {
+}
---
+++ src/test/java/com/abhishyam/learn/ThymeleafLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishyam.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
